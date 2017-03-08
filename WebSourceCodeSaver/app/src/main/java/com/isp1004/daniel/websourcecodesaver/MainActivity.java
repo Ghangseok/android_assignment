835e947d2cce5383c9080dcefbaf865fa7ff8805
Ghangseok <redeteus@gmail.com>
2017-03-08 15:37:06 +0900

    Add files via upload

---
+++ WebSourceCodeSaver/app/src/main/java/com/isp1004/daniel/websourcecodesaver/MainActivity.java
@@ -0,0 +1,13 @@
+package com.isp1004.daniel.websourcecodesaver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
